fe96b9b9a3cb944035eb17ad2dfabe2b5cd5dcd7
HP <HP@DESKTOP-P695FEI>
2025-02-21 02:19:00 +0500

    Add project files.

---
+++ Practicing C#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_C_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Hello World");
+        }
+    }
+}
