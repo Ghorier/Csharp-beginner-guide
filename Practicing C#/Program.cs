b9a98b327eafba0844bceb93fe47a63af8ec8e30
Ghorier <m.ahmedkashif057gamil.com>
2025-02-23 01:51:22 +0500

    Completed variable doing array

--- Practicing C#/Program.cs
+++ Practicing C#/Program.cs
@@ -10,7 +10,26 @@ namespace Practicing_C_
     {
         static void Main()
         {
-            Console.Write("Hello World");
+//llkk
+            
+            //Console.WriteLine(" Please enter the value of x ",X);
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            //string interpolation
+            Console.WriteLine($"Your name is {name}");
+            Console.WriteLine("Enter your age ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your age is " + age);
+            int[] number = { 20, 30, 40 };
+            Console.WriteLine(number[1]);
+            
+
+            
+
+            
+
         }
+
     }
 }
